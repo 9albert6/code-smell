885add4ed5ba5fc9c680143cd024ca0e6c5ff0e3
Albert Kulakowski <9albert6@gmail.com>
2020-01-31 15:50:59 +0100

    Deleted code smells and added bacis tests.

--- src/main/java/com/danix/code/smell/example001/Company.java
+++ src/main/java/com/danix/code/smell/example001/Company.java
@@ -35,6 +35,18 @@ public class Company extends Customer {
                 account.substract(Money.newInstance(money.getAmount(), money.getCurrency()));
             }
         }
+
+        if (account.isOverdraft()) {
+            double value = 0;
+            if (account.getType().isPremium()) {
+                value = money.getAmount() + money.getAmount() * account.overdraftFee() * companyOverdraftDiscount / 2;
+            } else {
+                value = money.getAmount() + money.getAmount() * account.overdraftFee() * companyOverdraftDiscount;
+            }
+            account.substract(Money.newInstance(value, money.getCurrency()));
+        } else {
+            account.substract(Money.newInstance(money.getAmount(), money.getCurrency()));
+        }
     }
 
     @Override
--- src/test/java/com/danix/code/smell/example001/CustomerTest.java
+++ src/test/java/com/danix/code/smell/example001/CustomerTest.java
@@ -1,6 +1,23 @@
 package com.danix.code.smell.example001;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.when;
 /**
  * @author  danix
  */
-public class CustomerTest { }
+public class CustomerTest {
+
+
+    @Test
+    public void shouldReturnName() {
+
+        Customer customer = Mockito.mock(Customer.class);
+        when(customer.getFullName()).thenReturn("Good");
+        assertEquals("Good", customer.getFullName());
+
+    }
+
+}
