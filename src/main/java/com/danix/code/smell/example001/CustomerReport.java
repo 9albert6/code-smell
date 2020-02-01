83a8ae515135a855eaec0e950c167eaaa99b2963
Albert Kulakowski <9albert6@gmail.com>
2020-02-01 21:03:42 +0100

    Changed name of the constant to capital letters.

--- src/main/java/com/danix/code/smell/example001/CustomerReport.java
+++ src/main/java/com/danix/code/smell/example001/CustomerReport.java
@@ -11,7 +11,7 @@ public class CustomerReport {
 
     private final Customer customer;
     private final Account account;
-    private static final String accountString = "Account: IBAN: ";
+    private static final String ACCOUNT_STRING = "Account: IBAN: ";
 
     public CustomerReport(@Nonnull final Customer customer, @Nonnull final Account account) {
         this.customer = checkNotNull(customer);
@@ -21,21 +21,21 @@ public class CustomerReport {
     public String printCustomerDaysOverdrawn() {
         String fullName = customer.getFullName();
 
-        String accountDescription = accountString + account.getIban() + ", Days Overdrawn: "
-                + account.getDaysOverdrawn();
+        String accountDescription = ACCOUNT_STRING + account.getIban() + ", Days Overdrawn: "
+                                    + account.getDaysOverdrawn();
         return fullName + accountDescription;
     }
 
     public String printCustomerMoney() {
         String fullName = customer.getFullName();
         String accountDescription = "";
-        accountDescription += accountString + account.getIban() + ", Money: " + account.getMoneyAmount();
+        accountDescription += ACCOUNT_STRING + account.getIban() + ", Money: " + account.getMoneyAmount();
         return fullName + accountDescription;
     }
 
     public String printCustomerAccount() {
-        return accountString + account.getIban() + ", Money: " + account.getMoneyAmount() + ", Account type: "
-                + account.getType();
+        return ACCOUNT_STRING + account.getIban() + ", Money: " + account.getMoneyAmount() + ", Account type: "
+               + account.getType();
     }
 
     public String printCustomer() {
